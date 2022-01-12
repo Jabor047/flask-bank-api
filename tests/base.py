baaf2e4082dff1f8e217af578a4c3bbe353a90c6
Jabor047 <gkkarobia@gmail.com>
2022-01-12 16:48:05 +0300

    added test files

---
+++ tests/base.py
@@ -0,0 +1,18 @@
+import unittest
+import requests
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from api.models.database import Accounts, Base, Transactions
+from api.logger import setup_logger
+from api.app import db
+from api import create_app
+
+class BaseTestCase(unittest.TestCase):
+    logger = setup_logger("unittest")
+    URL = "http://127.0.0.1:5000"
+
+    def the():
+        pass
+
