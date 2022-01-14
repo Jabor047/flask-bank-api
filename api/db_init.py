4250a457f189e70213c37a6d359dd211162c145d
Jabor047 <gkkarobia@gmail.com>
2022-01-14 15:33:22 +0300

    Took the database intialization into a seperate file

---
+++ api/db_init.py
@@ -0,0 +1,19 @@
+import os
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from api.models.database import Base
+
+
+# for linux and windows systems uncomment below
+docker_host_ip = "172.18.0.2"
+
+# for mac os
+# docker_host_ip = "host.docker.internal"
+engine = create_engine(f"postgresql://docker:docker@{docker_host_ip}/docker")
+
+Base.metadata.bind = engine
+
+db = scoped_session(sessionmaker(bind=engine))
