ca394e672f827ba6e57bb14ea17832c24d15a4c4
Jabor047 <gkkarobia@gmail.com>
2022-01-14 07:35:22 +0300

    added logging to test_api to help evalute failed tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -2,7 +2,8 @@ import unittest
 import requests
 import sys
 import os
-
+import json
+from pprint import pprint as pp
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from base import BaseTestCase
 
@@ -17,8 +18,12 @@ class BankApiTestCase(BaseTestCase):
         }
 
         response = requests.post(self.URL + '/create_account', params=params)
-        self.assertEqual(response.status_code, 200)
-        self.assertIn("account Successfully added", response.message)
+        response = response.content
+        response = response.decode("utf-8")
+        response = json.loads(response)
+        pp(response)
+        self.assertEqual(response["status_code"], 200)
+        self.assertIn("account Successfully added", response["message"])
 
     def test_create_account_customer_name_fail(self):
         params = {
@@ -30,8 +35,12 @@ class BankApiTestCase(BaseTestCase):
         }
 
         response = requests.post(self.URL + '/create_account', params=params)
-        self.assertEqual(response.status_code, 400)
-        self.assertIn("doesn't exist", response.message)
+        response = response.content
+        response = response.decode("utf-8")
+        response = json.loads(response)
+        pp(response)
+        self.assertEqual(response["status_code"], 400)
+        self.assertIn("doesn't exist", response["message"])
 
     def test_create_account_account_exists(self):
         params = {
@@ -43,8 +52,12 @@ class BankApiTestCase(BaseTestCase):
         }
 
         response = requests.post(self.URL + '/create_account', params=params)
-        self.assertEqual(response.status_code, 400)
-        self.assertIn("already exists", response.message)
+        response = response.content
+        response = response.decode("utf-8")
+        response = json.loads(response)
+        pp(response)
+        self.assertEqual(response["status_code"], 400)
+        self.assertIn("already exists", response["message"])
 
     def test_transfer_success(self):
         account_params = {
@@ -64,9 +77,12 @@ class BankApiTestCase(BaseTestCase):
         }
 
         response = requests.post(self.URL + "/transfer", params=transfer_params)
-
-        self.assertEqual(200, response.status_code)
-        self.assertIn("transfered from", response.message)
+        response = response.content
+        response = response.decode("utf-8")
+        response = json.loads(response)
+        pp(response)
+        self.assertEqual(200, response["status_code"])
+        self.assertIn("transfered from", response["message"])
 
     def test_transfer_insufficient_balance(self):
         transfer_params = {
@@ -76,8 +92,12 @@ class BankApiTestCase(BaseTestCase):
         }
 
         response = requests.post(self.URL + "/transfer", params=transfer_params)
-        self.assertEqual(response.status_code, 403)
-        self.assertIn("insufficient balance", response.message)
+        response = response.content
+        response = response.decode("utf-8")
+        response = json.loads(response)
+        pp(response)
+        self.assertEqual(response["status_code"], 403)
+        self.assertIn("insufficient balance", response["message"])
 
     def test_transfer_target_account_exists_not(self):
         transfer_params = {
@@ -87,8 +107,12 @@ class BankApiTestCase(BaseTestCase):
         }
 
         response = requests.post(self.URL + "/transfer", params=transfer_params)
-        self.assertEqual(response.status_code, 404)
-        self.assertIn("Target account", response.message)
+        response = response.content
+        response = response.decode("utf-8")
+        response = json.loads(response)
+        pp(response)
+        self.assertEqual(response["status_code"], 404)
+        self.assertIn("Target account", response["message"])
 
     def test_transfer_source_account_exists_not(self):
         transfer_params = {
@@ -98,8 +122,12 @@ class BankApiTestCase(BaseTestCase):
         }
 
         response = requests.post(self.URL + "/transfer", params=transfer_params)
-        self.assertEqual(response.status_code, 404)
-        self.assertIn("Source account", response.message)
+        response = response.content
+        response = response.decode("utf-8")
+        response = json.loads(response)
+        pp(response)
+        self.assertEqual(response["status_code"], 404)
+        self.assertIn("Source account", response["message"])
 
     def test_transfer_both_accounts_exist_not(self):
         transfer_params = {
@@ -109,8 +137,12 @@ class BankApiTestCase(BaseTestCase):
         }
 
         response = requests.post(self.URL + "/transfer", params=transfer_params)
-        self.assertEqual(response.status_code, 404)
-        self.assertIn("Both accounts not Found", response.message)
+        response = response.content
+        response = response.decode("utf-8")
+        response = json.loads(response)
+        pp(response)
+        self.assertEqual(response["status_code"], 404)
+        self.assertIn("Both accounts not Found", response["message"])
 
     def test_transfer_same_account_transfer(self):
         transfer_params = {
@@ -120,39 +152,61 @@ class BankApiTestCase(BaseTestCase):
         }
 
         response = requests.post(self.URL + "/transfer", params=transfer_params)
-        self.assertEqual(response.status_code, 403)
-        self.assertIn("Can't transfer to the same account", response.message)
+        response = response.content
+        response = response.decode("utf-8")
+        response = json.loads(response)
+        pp(response)
+        self.assertEqual(response["status_code"], 403)
+        self.assertIn("Can't transfer to the same account", response["message"])
 
     def test_retrive_balance_success(self):
         retrieve_balance_params = {
             "account_number": "0009"
         }
-        response = requests.post(self.URL + "/retrieve_balance", params=retrieve_balance_params)
-        self.assertEqual(response.status_code, 200)
-        self.assertIn("Account balance is", response.message)
+        response = requests.get(self.URL + "/retrieve_balance", params=retrieve_balance_params)
+        response = response.content
+        response = response.decode("utf-8")
+        response = json.loads(response)
+        pp(response)
+        self.assertEqual(response["status_code"], 200)
+        self.assertIn("Account balance is", response["message"])
 
     def test_retrive_balance_account_exists_not(self):
         retrieve_balance_params = {
             "account_number": "0008"
         }
-        response = requests.post(self.URL + "/retrieve_balance", params=retrieve_balance_params)
-        self.assertEqual(response.status_code, 404)
-        self.assertIn("Account not Found", response.message)
+        response = requests.get(self.URL + "/retrieve_balance", params=retrieve_balance_params)
+        response = response.content
+        response = response.decode("utf-8")
+        response = json.loads(response)
+        pp(response)
+        self.assertEqual(response["status_code"], 404)
+        self.assertIn("Account not Found", response["message"])
 
     def test_transferhistory_success(self):
         retrieve_balance_params = {
             "account_number": "0009"
         }
-        response = requests.post(self.URL + "/transferhistory", params=retrieve_balance_params)
-        self.assertEqual(response.status_code, 200)
+        response = requests.get(self.URL + "/transferhistory", params=retrieve_balance_params)
+        response = response.content
+        pp(response)
+        response = response.decode("utf-8")
+        response = json.loads(response)
+        pp(response)
+        self.assertEqual(response["status_code"], 200)
 
     def test_transferhistory_fail(self):
         retrieve_balance_params = {
             "account_number": "0009"
         }
-        response = requests.post(self.URL + "/transferhistory", params=retrieve_balance_params)
-        self.assertEqual(response.status_code, 404)
-        self.assertIn("Account not Found", response.message)
+        response = requests.get(self.URL + "/transferhistory", params=retrieve_balance_params)
+        response = response.content
+        pp(response)
+        response = response.decode("utf-8")
+        response = json.loads(response)
+        pp(response)
+        self.assertEqual(response["status_code"], 404)
+        self.assertIn("Account not Found", response["message"])
         self.delete_test_accounts()
 
     def delete_test_accounts(self):
